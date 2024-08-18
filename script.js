8173594aec73eac76015fa55ab038c73850de335
DanielNatanael <132743551+DanielNatanael@users.noreply.github.com>
2024-08-19 01:03:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Tambahkan animasi atau fungsi interaktif lainnya di sini
+    console.log("Portfolio loaded successfully!");
+});
